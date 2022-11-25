012ecbaed5f9ded29527b1a1127bcbc26e936d1e
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-25 19:34:32 +0700

    update PostResource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -9,7 +9,22 @@ class PostResource extends JsonResource
     // define properti
     public $status;
     public $message;
-    
+
+    /**
+     * __construct
+     *
+     * @param  mixed $status
+     * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status   = $status;
+        $this->message  = $message;
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -18,6 +33,10 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource,
+        ];
     }
 }
